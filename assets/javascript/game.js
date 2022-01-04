f714655fbb9f36d3d3a3cd54363e28c9185781cf
DennisMarkham <workdennis@yahoo.com>
2022-01-03 20:25:29 -0600

    Numbers reconfigured
    This time for real

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,210 @@
+var winSound = new Audio("assets/sounds/win.wav");
+var lossSound = new Audio("assets/sounds/loss.wav");
+var blingSound = new Audio("assets/sounds/bling.wav");
+lossSound.volume = 0.5;
+
+//sets a random goal number, set's player number.  Number is 11 - 100, thus always lower
+//than the crystal number
+var goalNumber = Math.floor(Math.random() * 90) + 11;
+var playerNumber = 0;
+var wins = localStorage.getItem("wins");
+var losses = localStorage.getItem("losses");
+
+//if there is no variable for wins or losses in local storage, it is set to zero
+//else, the "wins" from local storage is achieved and put into the wins variable in
+//the script
+if (wins === null) {
+  wins = 0;
+}
+ 
 
 
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <meta charset="UTF-8">
-    <title>Crystal Collector</title>
-<link rel="stylesheet" type="text/css" href="assets/css/sassitize.css">
+if (losses === null)
+{
+  losses = 0;
+}
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+console.log("Wins:" + wins);
+console.log("Losses:" + losses);
+//this value does not change!!!  WTF!?s
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+$("#wins").text(wins);
+
+$("#losses").text(losses);
+//okay, now it persists, but it ONLY updates after you refresh the page.  So...
 
 
+
+//gives a random value to blue, green, red, and purple.  Number will be 1 - 10.
+ var blue = Math.floor(Math.random() * 10) + 1;
+ var green = Math.floor(Math.random() * 10) + 1;
+ var red = Math.floor(Math.random() * 10) + 1;
+ var purple = Math.floor(Math.random() * 10) + 1; 
+
+
+//assigns value to the gem pics based on above random numbers
+$("#blueGem").attr("value", blue);
+$("#greenGem").attr("value", green);
+$("#redGem").attr("value", red);
+$("#purpleGem").attr("value", purple);
+
+
+//puts goal number variable in goal number box
+$("#goalNumber").text(goalNumber);
+
+//puts player number variable in player box
+$("#playerNumber").text(playerNumber);
+
+//puts wins in wins box (ccurrently at zero)
+// $("#wins").text(wins);
+
+// //puts losses in losses box
+// $("#losses").text(losses);
+
+
+//when you click one of the gems
+  $("img").click(function(){
+  //playerNumber equals whatever it was before plus the value of the gem
+  playerNumber = playerNumber + parseInt($(this).attr("value"));
+  
+
+  if (playerNumber == goalNumber)
+  {
+    //if the player number meets the goal number,
+    //a non-existent varaible called wins is incremented.
+    //see, how do I set up wins, without having it reset every refresh!?
+    //So maybe check if there's already a value in local storage, and if not, assign it to
+    //zero.
+    //So at the top of the page "if wins in localstorage = true, wins = localStorage.getItem(wins)"
+    //else, wins = 0.
+    wins++;
+    winSound.play();
+    localStorage.setItem("wins", wins);
     
-  </head>
-  <body>
-  <div id = "whole">
-  <h1>Crystal Collector</h1>  
+    var x = screen.width/2 - 500/2;
+    var y = screen.height/2 - 350/2;
+
+    if(window.innerWidth > 800)
+    {
+    window.open("win.html", "_blank",'height=385,width=500,left='+x+',top='+y);
+  }
+
+//if you win, goal number is reset, player number is reset, and so are gem values
+goalNumber = Math.floor(Math.random() * 100) + 1;
+playerNumber = 0;
+
+
+ blue = Math.floor(Math.random() * 10) + 1;
+ green = Math.floor(Math.random() * 10) + 1;
+ red = Math.floor(Math.random() * 10) + 1;
+ purple = Math.floor(Math.random() * 10) + 1; 
+
+$("#blueGem").attr("value", blue);
+$("#greenGem").attr("value", green);
+$("#redGem").attr("value", red);
+$("#purpleGem").attr("value", purple);
+    
+    $("#wins").text(wins);
+  }
+else if (playerNumber > goalNumber)
+  {
+    losses++;
+    lossSound.play();
+
+    localStorage.setItem("losses", losses);
   
-  <div class = "grayBox" id = "instructions">
-  <p>Each crystal has been given a random number value.  Click the crystal to add
-  its value to your total.  If you can match the goal, you win.  If you exceed it,
-  you will lose.  Eitherway, the game will restart. <b>New Feature: Wins and losses will be tracked persistently!
-  However, feel free to clear your stats at any time.</b></p>
-  </div>
+    var x = screen.width/2 - 500/2;
+    var y = screen.height/2 - 350/2;
 
-  <div class = "grayBox" id = "playerNumberBox">
-  <span id = "playerNumber">
-  </span>
-  </div>
+    if (window.innerWidth > 800)
+    {
+    window.open("loss.html", "_blank",'height=385,width=500,left='+x+',top='+y);
+  }
 
-  <div class = "grayBox" id = "goalNumberBox">
-  <span id = "goalNumber">
-  </span>
-</div>
+//if you lose, all those numbers are reset as well.     
+goalNumber = Math.floor(Math.random() * 100) + 1;
+playerNumber = 0;
+
+blue = Math.floor(Math.random() * 10) + 1;
+green = Math.floor(Math.random() * 10) + 1;
+red = Math.floor(Math.random() * 10) + 1;
+purple = Math.floor(Math.random() * 10) + 1; 
+
+$("#blueGem").attr("value", blue);
+$("#greenGem").attr("value", green);
+$("#redGem").attr("value", red);
+$("#purpleGem").attr("value", purple);
+
+$("#losses").text(losses);
+  }
+
+  else {
+    blingSound.pause();
+    blingSound.play();
+    
+  }
+
+//this displays the playerNumber and goalNumber in their appropriate divs after each click
+$("#goalNumber").text(goalNumber);
+
+$("#playerNumber").text(playerNumber);
+
+// $("#wins").text(wins);
+
+// $("#losses").text(losses);
+
+
+
+
+
+
+//this extracts them from local storage to be printed.
+// $("#wins").text(wins);
+
+// $("#losses").text(losses);
+
+//now the problem is once you hit refresh, wins and losses go back to zero.  Is that because
+//the computer starts reading from the top again?
+
+
 
   
-  
- 
- <div id="gems">
- <img id ="blueGem" class = "gem" src = "assets/images/blueGem.png" alt = "blue gem">
- <img id ="greenGem" class = "gem" src ="assets/images/greenGem.png" alt = "green gem">
- <img id = "redGem" class = "gem" src = "assets/images/redGem.png" alt = "red gem">
- <img id = "purpleGem" class = "gem" src = "assets/images/purpleGem.png" alt = "purple gem">
-</div>
 
-<div class = "grayBox" id ="winLossBox">
-Wins: <span id = "wins">0
-  </span>
-  <br>
+});
 
-Losses: <span id = "losses">0
-  </span>
-</div>
-<br>
-<button id = "clear"> Clear Stats?</button>
-</div>
 
-<script src="assets/javascript/game.js"></script>
- 
-  </body>
-</html>
+  $("button").click(function(){
+    confirm("Are you sure you want to delete your stats?");
+
+    if(confirm)
+    {
+      localStorage.clear();
+      //it's just as if nothing happens
+      console.log("Local storage is supposed to be cleared");
+      console.log(localStorage.getItem("losses"));
+      //so it seems to work, but the text doesn't update, unless you hit refresh
+      // $("#losses").text("0");
+      // $("#wins").text("0");
+      //yet even writing it this way doesn't work, wtf!?
+      //okay, let's try manually refreshing the page.
+      location.reload();
+
+    }
+  })
+
+//WINDOW SCRIPTS
+
+function closeWindow()
+{
+  window.close();
+}
+
+$(".exit").click(function(){
+     closeWindow();
+});
+
+function closeTimer(){
+  setTimeout(closeWindow(), 50000);
+  //that's weird, it disappears instantly
+}
+//*****
